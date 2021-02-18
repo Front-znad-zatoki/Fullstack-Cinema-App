08e38a3954f3b9d32e97d7bd85e8afdf6a1788a3
VieraBoschkova <vieraboschkova@gmail.com>
2021-02-19 00:09:10 +0100

    init: basic npm packages

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+// const connectDB = require('./config/db');
+
+
+const app = express();
+
+app.get('/', (req, res) => res.send('API Runnin'))
+
+// Connect Database
+// connectDB();
+
+// Init Middleware
+app.use(express.json());
+
+// Define Routes
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    console.log('production')
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
