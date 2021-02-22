8342a42763e7a4f672fa377dbbc2cad209fe99e5
paula0403 <paulinalipinskagdy@gmail.com>
2021-02-22 16:15:46 +0100

    CinemaHall has been transferred, package.json changed

--- api/cinemaHall/CinemaHall.js
+++ api/cinemaHall/CinemaHall.js
@@ -1,1 +1,9 @@
 //CinemaHallSchema
+const mongoose = require('mongoose');
+
+const CinemaHallSchema = new mongoose.Schema({
+  numberOfHall: Number,
+  numberOfSeats: Number,
+  numberOfRows: Number,
+});
+module.exports = mongoose.model('cinemaHall', CinemaHallSchema);
