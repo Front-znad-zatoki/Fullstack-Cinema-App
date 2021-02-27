1185ebcc3f5d5855c95c46ce8a1bc32736e477b9
VieraBoschkova <vieraboschkova@gmail.com>
2021-02-27 11:30:49 +0100

    feat: add status to response

--- api/user/userMeRoute.js
+++ api/user/userMeRoute.js
@@ -11,7 +11,7 @@ const router = express.Router();
 router.get('/', authMiddleware, async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select('-password');
-    res.json(user);
+    res.status(200).res.json(user);
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
