d3cce45bf3c8f0e20f951f16a3942c0b4094db7d
VieraBoschkova <vieraboschkova@gmail.com>
2021-02-28 21:58:21 +0100

    feat: add basic login form structure

--- client/src/domain/Auth/Login/index.js
+++ client/src/domain/Auth/Login/index.js
@@ -1,7 +1,64 @@
 import './style.scss';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Login() {
-  return <div className="login">Rendering Login in here</div>;
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+    console.log(email, password);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(email, password);
+  };
+
+  // TODO: add context to retrieve info if the user is already authenticated
+  // if (isAuthenticated) {
+  //   return <Redirect to="/user/me" />;
+  // }
+
+  return (
+    <div className="auth">
+      <h1>Login</h1>
+      <form className="auth__form" onSubmit={onSubmit}>
+        <div className="auth__form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={onChange}
+            required
+          />
+        </div>
+        <div className="auth__formgroup">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={onChange}
+            minLength="5"
+          />
+        </div>
+        {/* TODO: add submit logic */}
+        <button type="submit" className="button--submit">
+          Login
+        </button>
+      </form>
+      <p className="auth__redirect">
+        Don't have an account? <Link to="/signup">Sign Up</Link>
+      </p>
+    </div>
+  );
 }
 
 export default Login;
