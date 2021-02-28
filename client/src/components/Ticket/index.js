d500e32ffa147858dc29f5a0ae06a8bc7159be1b
VieraBoschkova <vieraboschkova@gmail.com>
2021-02-28 21:18:58 +0100

    feat: add basic ticket features

---
+++ client/src/components/Ticket/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Ticket({ seatNr, type, price }) {
+  return (
+    <div className="ticket">
+      <h4 className="ticket__heading">{seatNr}</h4>
+      <div className="ticket__details">
+        <p className="ticket__type">{type}</p>
+        <p className="ticket__price">{price}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Ticket;
