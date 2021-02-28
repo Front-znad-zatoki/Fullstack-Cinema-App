a54a3347612141f4b194d2b21af62f14bfd81032
VieraBoschkova <vieraboschkova@gmail.com>
2021-02-28 11:02:36 +0100

    lint feat: add rimraf

--- api/movie/movieRoute.js
+++ api/movie/movieRoute.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import Movie from './Movie.js';
+
 const router = express.Router();
 router
   .route('/')
--- api/seat/Seat.js
+++ api/seat/Seat.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
-const Schema = mongoose.Schema;
+
+const { Schema } = mongoose;
 
 const seat = new Schema({
   hall: {
