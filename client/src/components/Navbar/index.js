e20e487960d9a1fb5d03fb114ff355bd784f8724
Aleksandra Pawlak <synowa8@gmail.com>
2021-03-02 17:45:42 +0100

    Implement Navbar component
    closes #34

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,12 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import './style.scss';
 import { Link } from 'react-router-dom';
 
-function Navbar() {
+function Navbar({ name, icon }) {
   return (
     <div className="navbar">
-      Rendering Navbar in here
-      <h1>Navigation</h1>
+      <h1>
+        <span className={icon} /> {name}
+      </h1>
       <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
         <li>
           <Link to="/movies">Movies</Link>
         </li>
@@ -20,5 +26,15 @@ function Navbar() {
     </div>
   );
 }
+
+Navbar.propTypes = {
+  name: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+};
+
+Navbar.defaultProps = {
+  name: 'FZZ Cinemas',
+  icon: 'fas fa-video',
+};
 
 export default Navbar;
