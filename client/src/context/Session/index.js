577b4857c134ee424fbfccc430aa5529c16ad2f2
VieraBoschkova <vieraboschkova@gmail.com>
2021-03-04 08:30:20 +0100

    test

---
+++ client/src/context/Session/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const SessionContext = (props) => {
+  return <div>Something</div>;
+};
+
+SessionContext.propTypes = {};
+
+export default SessionContext;
