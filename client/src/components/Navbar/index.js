8088dfe94d0483ffd2f227a07f6b07e21230dd44
rednaxela700 <ohleg@windowslive.com>
2021-03-06 08:31:56 +0100

    add routes

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './style.scss';
 import { Link } from 'react-router-dom';
+import { ROUTE_ROOT, ROUTE_MOVIES, ROUTE_SIGNUP, ROUTE_LOGIN } from '../../../routes';
 
 function Navbar() {
   return (
@@ -10,16 +11,16 @@ function Navbar() {
       </h1>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to={ROUTE_ROOT}>Home</Link>
         </li>
         <li>
-          <Link to="/movies">Movies</Link>
+          <Link to={ROUTE_MOVIES}>Movies</Link>
         </li>
         <li>
-          <Link to="/signup">SignUp</Link>
+          <Link to={ROUTE_SIGNUP}>SignUp</Link>
         </li>
         <li>
-          <Link to="/login">Login</Link>
+          <Link to={ROUTE_LOGIN}>Login</Link>
         </li>
       </ul>
     </nav>
