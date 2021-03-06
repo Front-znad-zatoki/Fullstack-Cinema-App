99bbcd1746e1cd6023ef63514ea2136de1170802
VieraBoschkova <vieraboschkova@gmail.com>
2021-03-06 21:48:20 +0100

    update files after merge

--- client/src/domain/MovieList/index.js
+++ client/src/domain/MovieList/index.js
@@ -1,12 +1,22 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import './style.scss';
 import moviesMock from '../../mock/moviesMock';
 import Movie from './Movie';
+import { ThemeContext } from '../../context/Theme';
+import AppTheme from '../../context/Theme/themeColors';
 
 function MovieList() {
   const [movies, setMovies] = useState(moviesMock);
+  const theme = useContext(ThemeContext)[0];
+  const currentTheme = AppTheme[theme];
   return (
-    <div className="movie__list-container">
+    <div
+      className="movie__list-container"
+      style={{
+        backgroundColor: `${currentTheme.backgroundColor}`,
+        color: `${currentTheme.textColor}`,
+      }}
+    >
       <ul className="movie__list">
         {movies.map((movie) => {
           return <Movie key={movie.id} movie={movie} />;
