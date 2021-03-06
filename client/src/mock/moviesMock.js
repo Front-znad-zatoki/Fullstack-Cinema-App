bec37e17a5d1794cf71a375a74df31d9e44fd5f4
VieraBoschkova <vieraboschkova@gmail.com>
2021-03-06 20:56:36 +0100

    feat: prepare mock data for testing context

---
+++ client/src/mock/moviesMock.js
@@ -0,0 +1,31 @@
+const movies = [
+  {
+    title: 'No Time to Die',
+    duration: 163,
+    releaseDate: '30 September 2021 (UK)',
+    description:
+      'James Bond has left active service. His peace is short-lived when Felix Leiter, an old friend from the CIA, turns up asking for help, leading Bond onto the trail of a mysterious villain armed with dangerous new technology.',
+    genre: ['Action', 'Adventure', 'Thriller'],
+    poster: '../assets/moviePosters/noTimeToDiePoster.jpg',
+  },
+  {
+    title: 'Time to Die',
+    duration: 13,
+    releaseDate: '30 September 2020 (UK)',
+    description:
+      'James Bond has left active service. His peace is short-lived when Felix Leiter, an old friend from the CIA, turns up asking for help, leading Bond onto the trail of a mysterious villain armed with dangerous new technology.',
+    genre: ['Action'],
+    poster: '../assets/moviePosters/avatar.jpg',
+  },
+  {
+    title: 'Die',
+    duration: 160,
+    releaseDate: '30 September 2010 (UK)',
+    description:
+      'James Bond has left active service. His peace is short-lived when Felix Leiter, an old friend from the CIA, turns up asking for help, leading Bond onto the trail of a mysterious villain armed with dangerous new technology.',
+    genre: ['Action', 'Adventure', 'Thriller'],
+    poster: '../assets/moviePosters/adamsApples.jpg',
+  },
+];
+
+export default movies;
