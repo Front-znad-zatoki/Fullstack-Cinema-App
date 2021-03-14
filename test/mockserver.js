93934730f5e7b3954f3bb61a77e8f3199aacb2e8
VieraBoschkova <vieraboschkova@gmail.com>
2021-03-14 20:02:12 +0100

    refactor tests

--- test/mockserver.js
+++ test/mockserver.js
@@ -1,11 +1,5 @@
 import express from 'express';
-import userRoute from '../api/user/userRoute.js';
 import movieRoute from '../api/movie/movieRoute.js';
-import orderRoute from '../api/order/orderRoute.js';
-import screeningRoute from '../api/screening/screeningRoute.js';
-import ticketRoute from '../api/ticket/ticketRoute.js';
-import cinemaRoute from '../api/cinema/cinemaRoute.js';
-import cinemaHallRoute from '../api/cinemaHall/cinemaHallRoute.js';
 
 const app = express();
 
@@ -13,13 +7,6 @@ const app = express();
 app.use(express.json());
 
 // Define Routes
-app.use('/api/users', userRoute);
 app.use('/api/movies', movieRoute);
-app.use('/api/orders', orderRoute);
-
-app.use('/api/screenings', screeningRoute);
-app.use('/api/tickets', ticketRoute);
-app.use('/api/cinemas', cinemaRoute);
-app.use('/api/cinemaHalls', cinemaHallRoute);
 
 export default app;
