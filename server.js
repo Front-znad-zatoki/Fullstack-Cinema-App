d95a3afaf544cf2c9524d4e47727978e3740dd47
VieraBoschkova <vieraboschkova@gmail.com>
2021-04-13 11:06:48 +0200

    fix

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import connectDB from './config/mongodb.js';
 import userRoute from './api/user/userRoute.js';
 import movieRoute from './api/movie/movieRoute.js';
@@ -11,6 +13,8 @@ import cinemaRoute from './api/cinema/cinemaRoute.js';
 import cinemaHallRoute from './api/cinemaHall/cinemaHallRoute.js';
 import seatRoute from './api/seat/seatRoute.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 dotenv.config();
 
 const app = express();
@@ -36,12 +40,15 @@ app.use('/api/cinemaHalls', cinemaHallRoute);
 app.use('/api/seats', seatRoute);
 
 // Serve static assets in production
-// if (process.env.NODE_ENV === 'production') {
-//   console.log('production');
-// app.use(express.static('client/build'))
-// TODO: finish deployment config
-// }
-console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, 'client', 'build', 'index.html'),
+    );
+  });
+}
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
