707f588421c392eca8e9c2cdc600ca003df9fd26
VieraBoschkova <vieraboschkova@gmail.com>
2021-04-13 21:23:23 +0200

    feat: change elements disposition

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,16 +4,22 @@ import { CinemaContext } from '../../context/Cinema';
 
 export default function Footer() {
   const { currentCinema } = useContext(CinemaContext);
+  console.log(currentCinema);
   return (
     <footer className="footer">
-      <h5 className="footer__header">CODERSCAMP 2020</h5>
       <ul className="footer__list">
+        <a href="https://coderscamp.edu.pl/" className="footer__item-detail">
+          <h5 className="footer__header">CODERSCAMP 2020</h5>
+        </a>
         <li className="footer__item">
           <h6 className="footer__item-header">Address</h6>
           <p className="footer__item-detail">
             {currentCinema.country}, {currentCinema.city}
           </p>
           <p className="footer__item-detail">{currentCinema.street}</p>
+          <p className="footer__item-detail">
+            {`${currentCinema.hours.open}:00 - ${currentCinema.hours.close}:00`}
+          </p>
         </li>
         <li className="footer__item">
           <h6 className="footer__item-header">Contact</h6>
@@ -33,18 +39,21 @@ export default function Footer() {
         <li className="footer__item">
           <h6 className="footer__item-header">Authors</h6>
           <a href="https://github.com/synowa" className="footer__item-detail">
+            <span className="fab fa-github" />
             Ola
           </a>
           <a
             href="https://github.com/paula0403"
             className="footer__item-detail"
           >
+            <span className="fab fa-github" />
             Paulina
           </a>
           <a
             href="https://github.com/vieraboschkova"
             className="footer__item-detail"
           >
+            <span className="fab fa-github" />
             Weronika
           </a>
         </li>
