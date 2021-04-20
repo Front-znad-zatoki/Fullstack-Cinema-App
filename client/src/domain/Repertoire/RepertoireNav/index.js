0be0b2d72cd89a6d0a47a9fd21001f1c2509295f
paula0403 <paulinalipinskagdy@gmail.com>
2021-04-20 12:10:02 +0200

    fix:styles changed

--- client/src/domain/Repertoire/RepertoireNav/index.js
+++ client/src/domain/Repertoire/RepertoireNav/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import './style.scss';
 import DatePicker from 'react-date-picker';
 import 'react-date-picker/dist/DatePicker.css';
 import CinemaForm from '../../../components/Navbar/CinemaForm';
+import './style.scss';
 
 function RepertoireNav({ selectedDate, setSelectedDate }) {
   const handleDate = (date) => {
